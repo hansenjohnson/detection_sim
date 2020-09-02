5e69a0018e71a0dcc0217bb113f9204d9e9ecb0a
Valentina Ceballos <valentinaceballos@Valentinas-MacBook-Air.local>
2020-09-02 15:43:45 -0300

    Created animations of whales calling and surfacing

--- tests/animation.R
+++ tests/animation.R
@@ -9,60 +9,88 @@ library(tidyverse)
 library(gifski)
 library(gganimate)
 source('r/rw_sim.R')
-source('r/detect_calls.R')
 
 # process -----------------------------------------------------------------
 
 # run model for multiple right whales
 # model parameters
-hrs = 24*7 # model run length (hr)
-nt = 60 # time res (s)
-nrws = 25
+res = 60 # time res (s)
 
-# run model
-whs = rw_sims(nrws = nrws, hrs = hrs, bh = 'feeding', nt = nt)
+# run models
+set.seed(1)
+whs = rw_sims(nrws = 10, hrs = 24, nt = res, bh = 'feeding')
 
-# coordinates of detector
-x_dt = 20
-y_dt = -40
+set.seed(1)
+wh = rw_sim(hrs = 24, nt = res, sub = TRUE, bh = 'feeding') %>%
+  mutate(
+    x=x/1000,
+    y=y/1000,
+    r=r/1000
+  )
 
-# make data frame using whale movement variables
-df = tibble(
-  id = whs$id,
-  t = whs$t,
-  x_whs = whs$x,
-  y_whs = whs$y,
-  x_dt,
-  y_dt,
-  r_whs = sqrt((x_whs-x_dt)^2 + (y_whs-y_dt)^2),
-  call = whs$call
-)
+# modify time to hours for animation
+whs$time = whs$time/60/60
+wh$time = wh$time/60/60
 
-# subset to only times with calls
-calls = df %>% filter(call==1)
+# # coordinates of detector
+# x_dt = 20
+# y_dt = -40
 
-# apply detection function to the call positions to extract probabilities of detection
-calls$p = detection_function(x = calls$r_whs)
-
-# generate a binomial distribution to see if each call was detected using this probability
-calls$detected = as.character(rbinom(n = nrow(calls), size = 1, prob = calls$p))
+# # make data frame using whale movement variables
+# df = tibble(
+#   id = whs$id,
+#   t = whs$t,
+#   x_whs = whs$x,
+#   y_whs = whs$y,
+#   x_dt,
+#   y_dt,
+#   r_whs = sqrt((x_whs-x_dt)^2 + (y_whs-y_dt)^2),
+#   call = whs$call
+# )
+# 
+# # subset to only times with calls
+# calls = df %>% filter(call==1)
+# 
+# # apply detection function to the call positions to extract probabilities of detection
+# calls$p = detection_function(x = calls$r_whs)
+# 
+# # generate a binomial distribution to see if each call was detected using this probability
+# calls$detected = as.character(rbinom(n = nrow(calls), size = 1, prob = calls$p))
 
 # plot tracks
-p = ggplot(data=df, aes(x=x_whs,y=y_whs, group=id))+
-  geom_path(alpha = 0.5, color = 'grey')+
-  geom_point(alpha = 0.7, shape=21, size = 2, fill = 'black')+
-  geom_point(data = calls,aes(x=x_whs,y=y_whs,fill=detected,size=detected),shape=21,alpha=0.7)+
+p = ggplot()+
+  geom_path(data=whs, aes(x=x,y=y, group=id,color=surface))+
+  scale_color_manual(values = c('0'='grey', '1'='black'))+
+  geom_point(data=filter(whs,call==1), aes(x=x,y=y), shape = 21, fill = 'red')+
   coord_equal()+
   labs(x = 'Easting (km)', y = 'Northing (km)')+
   theme_bw()+
   theme(panel.grid = element_blank())
 p
 
+p2 = ggplot()+
+  geom_path(data=wh, aes(x=x,y=y, group=dive_index,color=surface))+
+  scale_color_manual(values = c('0'='grey', '1'='black'))+
+  geom_point(data=filter(wh,call==1), aes(x=x,y=y), shape = 21, fill = 'red')+
+  coord_equal()+
+  labs(x = 'Easting (km)', y = 'Northing (km)')+
+  theme_bw()+
+  theme(panel.grid = element_blank())
+p2
+
 # create animation
 anim = p + 
-  transition_reveal(along = t)+
+  transition_reveal(along = time)+
   ease_aes('linear')+
   ggtitle("Hours: {round(frame_along,0)}")
 
-# render animation (might take some time)
+anim2 = p2 + 
+  transition_reveal(along = time)+
+  ease_aes('linear')+
+  ggtitle("Hours: {round(frame_along,0)}")
+
+# render animation and save (might take some time)
 anim
+anim_save('figures/multiple_whales_animation.gif', animation=anim)
+anim2
+anim_save('figures/one_whale_animation.gif', animation=anim2)
