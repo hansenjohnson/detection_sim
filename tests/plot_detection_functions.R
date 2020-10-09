2ec2445f561e9ccb4c6b72bbe9e97df1fbc8c78d
Hansen Johnson <hansen.johnson@dal.ca>
2020-10-09 13:43:27 -0300

    add facet to detection function plot

--- tests/plot_detection_functions.R
+++ tests/plot_detection_functions.R
@@ -5,18 +5,18 @@ library(tidyverse)
 source('r/rw_sim.R')
 
 # generate vector of ranges
-r = seq(from = 0, to = 40, by = 0.1)
+
 
 # acoustic detection function
 aco = tibble(
-  r,
+  r = seq(from = 0, to = 30, by = 0.1),
   p = detection_function(x = r, L = 1.045, x0 = 10, k = -0.3),
   type = 'acoustic'
 )
 
 # visual detection function
 vis = tibble(
-  r,
+  r = seq(from = 0, to = 3, by = 0.1),
   p = detection_function(x = r, L = 1, x0 = 1, k = -4.8),
   type = 'visual'
 )
@@ -24,8 +24,14 @@ vis = tibble(
 # combine
 df = rbind(aco,vis)
 
-# plot
-ggplot(df,aes(x=r,y=p,group=type,color=type))+
+# same plot
+p1 = ggplot(df,aes(x=r,y=p,group=type,color=type))+
   geom_path()+
   labs(x='Range (km)', y='Probability', color = 'Method')+
+  # scale_x_continuous(limits = c(0,30))+
   theme_bw()
+
+# faceted
+p2 = p1+facet_wrap(~type, scales = "free_x")
+
+
