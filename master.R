283f1d0234424827655890c9b7aa8b2534235333
Hansen Johnson <hansen.johnson@dal.ca>
2020-10-09 18:08:55 -0300

    update master with 1 week example

--- master.R
+++ master.R
@@ -1,2 +1,23 @@
 ## master ##
 # master script to reproduce detection_sim analysis
+
+# setup -------------------------------------------------------------------
+
+# read in functions
+source('r/rw_sim.R')
+
+# process -----------------------------------------------------------------
+
+# run 1 week model
+source('r/run_one_week.R')
+
+# run diagnostics
+source('r/run_diagnostics.R')
+
+#  plot -------------------------------------------------------------------
+
+# detection function
+source('r/plot_detection_functions.R')
+
+# 1 week example
+source('r/plot_one_week.R')
