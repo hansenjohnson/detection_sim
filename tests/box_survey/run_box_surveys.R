26b93c4fd9778ac6dfe69d9c34c62199a80bc970
Valentina Ceballos <valentinaceballos@Valentinas-MacBook-Air.local>
2020-12-20 11:14:45 -0500

    Testing new box survey w/ different numbers of surveys

--- tests/box_survey/run_box_surveys.R
+++ tests/box_survey/run_box_surveys.R
@@ -13,10 +13,10 @@ width=12
 n_surveys=50          
 
 # numbers of whales to test
-nrwsl = c(1,3,10,25) 
+nrwsl = c(1, 3, 5, 10, 25, 50) 
 
-# behaviors to test
-bhl = c('feeding','socializing') 
+# numbers of surveys to test
+#snl = c(10, 50) 
 
 # setup -------------------------------------------------------------------
 
@@ -25,18 +25,29 @@ source('tests/box_survey/box_survey_functions.R')
 # process -----------------------------------------------------------------
 
 # run surveys
-DF = vector('list', length = length(nrwsl)*length(bhl))
-cnt = 1
-for(ii in seq_along(bhl)){
+DF = vector('list', length = length(nrwsl))
+#cnt = 1
+#for(ii in seq_along(snl)){
   for(jj in seq_along(nrwsl)){
-    message('Simulating surveys with ', nrwsl[jj], ' in ',  bhl[ii], ' behavior (run ', cnt, ' of ', length(DF), ')')
-    DF[[cnt]] = run_box_surveys(height=height,width=width,nrws=nrwsl[jj],n_surveys=n_surveys,bh=bhl[ii])
-    cnt = cnt+1
+    message('Simulating surveys with ', nrwsl[jj], ' whale(s)')
+    DF[[jj]] = run_box_surveys(height=height,width=width,nrws=nrwsl[jj],n_surveys=n_surveys)
+    #cnt = cnt+1
   }
-}
+#}
 
 # combine
 df = bind_rows(DF)
 
 # save
 saveRDS(df, file = 'tests/box_surveys.rds')
+
+# plot --------------------------------------------------------------------
+#df$type = paste(df$platform,df$behavior)
+  
+p = ggplot()+
+  geom_path(data = df, aes(x = n_whales, y = transit_p, color = platform, group = platform))+
+  labs(x = 'number of surveys', y = 'probability of detection')+
+  #facet_grid(~n_whales)+
+  theme_bw()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+p
