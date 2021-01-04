23670ccb6423b73af74374ff3d50605a90850ff1
Hansen Johnson <hansen.johnson@dal.ca>
2021-01-04 12:08:35 -0500

    add updated box survey functions and master to `r/` for remote run

---
+++ r/run_box_surveys.R
@@ -0,0 +1,38 @@
+## run_box_surveys ##
+# run box surveys and save output
+
+# numbers of right whales in box
+n_whales = c(1, 5, 10, 25, 50, 75)
+
+# number of surveys to run for each combination of platform, n_whale, etc. 
+n_surveys = 10
+
+# dimensions of TC box (km)
+tc_height = 20
+tc_width = 100
+
+# dimensions of DFO box (km)
+dfo_height = 18
+dfo_width = 12
+
+# output file
+ofile = 'data/processed/box_surveys.rds'
+
+# setup -------------------------------------------------------------------
+
+# read in functions
+source('r/box_survey_functions.R')
+
+# process -----------------------------------------------------------------
+
+# run DFO and TC models
+dfo = run_box_surveys(height = dfo_height, width = dfo_width, nrws = n_whales, n_surveys = n_surveys) %>%
+  mutate(box_type = 'DFO')
+tc = run_box_surveys(height = tc_height, width = tc_width, nrws = n_whales, n_surveys = n_surveys) %>%
+  mutate(box_type = 'TC')
+
+# combine output
+df = rbind(dfo, tc)
+
+# save
+saveRDS(df, ofile)
