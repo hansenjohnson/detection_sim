66bb57152cd501e069bdb598d3b2c1191f5e119c
Hansen Johnson <hansen.johnson@dal.ca>
2021-01-04 12:56:05 -0500

    apply nice formatting

--- r/run_box_surveys.R
+++ r/run_box_surveys.R
@@ -1,7 +1,9 @@
 ## run_box_surveys ##
 # run box surveys and save output
 
-# number of surveys to run for each combination of platform, n_whale, etc. 
+# input -------------------------------------------------------------------
+
+# number of surveys to run for each combination of platform, n_whale, etc.
 n_surveys = 100
 
 # numbers of right whales in box
@@ -25,18 +27,41 @@ source('r/box_survey_functions.R')
 
 # make data directory
 data_dir = dirname(ofile)
-if(!dir.exists(data_dir)){dir.create(data_dir, recursive = TRUE)}
+if (!dir.exists(data_dir)) {
+  dir.create(data_dir, recursive = TRUE)
+}
 
 # process -----------------------------------------------------------------
 
-# run DFO and TC models
-dfo = run_box_surveys(height = dfo_height, width = dfo_width, n_whales = n_whales, n_surveys = n_surveys) %>%
-  mutate(box_type = 'DFO')
-tc = run_box_surveys(height = tc_height, width = tc_width, n_whales = n_whales, n_surveys = n_surveys) %>%
-  mutate(box_type = 'TC')
+message('\n###############################')
+message('## NARW DETECTION SIMULATION ##')
+message('###############################\n')
+
+# run DFO simulation
+dfo = run_box_surveys(
+  box_type = 'DFO',
+  height = dfo_height,
+  width = dfo_width,
+  n_whales = n_whales,
+  n_surveys = n_surveys
+)
+
+# run TC simulation
+tc = run_box_surveys(
+  box_type = 'TC',
+  height = tc_height,
+  width = tc_width,
+  n_whales = n_whales,
+  n_surveys = n_surveys
+)
 
 # combine output
 df = rbind(dfo, tc)
 
 # save
 saveRDS(df, ofile)
+
+message('\n#############################')
+message('## SIMULATION COMPLETE :)  ##')
+message('#############################\n')
+message('Data saved as: ', ofile)
