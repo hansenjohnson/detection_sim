69272fd92039cf96865c2993ddcb3e47ed4b72a7
Hansen Johnson <hansen.johnson@dal.ca>
2021-01-17 18:12:28 -0500

    increase to 1000 surveys for kaos run

--- r/run_box_surveys.R
+++ r/run_box_surveys.R
@@ -4,7 +4,7 @@
 # input -------------------------------------------------------------------
 
 # number of surveys to run for each combination of platform, n_whale, etc.
-n_surveys = 100
+n_surveys = 1000
 
 # numbers of right whales in box
 n_whales = c(seq(1, 10, 1), seq(15, 65, 5))
