459e00b6a899bdbabfc7c54ff9fa772377388073
Valentina Ceballos <valentinaceballos@Valentinas-MacBook-Air.local>
2021-05-31 15:59:38 -0400

    Learning to add buffer to platform survey tracks

---
+++ tests/rpas/buffer_tracks.R
@@ -0,0 +1,45 @@
+## buffer_tracks ##
+# tests and practice with the sf package to buffering platform track lines
+
+# setup -------------------------------------------------------------------
+
+set.seed(123)
+source('tests/rpas/box_survey_functions.R')
+library(sf)
+
+# process -----------------------------------------------------------------
+
+# create plane survey track
+trk = simulate_track(platform='plane',res=2.5,ymax=18,ymin=0,xmax=12,xmin=0)
+
+# plot plane survey track
+p = ggplot()+
+  geom_path(data=trk,aes(x=x,y=y))+
+  labs(x = 'Easting (km)', y = 'Northing (km)')+
+  theme_bw()+
+  theme(panel.grid = element_blank())
+p
+
+# make list of point coordinates from platform track line
+points_coords <- data.frame(x=trk$x,
+                            y=trk$y)
+
+# make a spatial lines object (turn coordinates into a line)
+lines_sp <- SpatialLines(list(Lines(Line(points_coords), ID=1)))
+summary(lines_sp)
+# plot
+q <- plot(points_coords, col="red")
+q <- q + plot(lines_sp, col="blue", add=TRUE)
+
+# buffer line and plot
+lines_buffer_sp <- gBuffer(lines_sp, width = 1)
+r <- plot(points_coords, col="red")
+r <- r + plot(lines_buffer_sp, border="red", lty="dashed", add=TRUE)
+r <- r + plot(lines_sp, col="blue", add=TRUE)
+
+# reintegrate into functions ----------------------------------------------
+
+# add attribute (time column) to the spatial points object
+lines_df <- data.frame(time=c(trk$time))
+lines_spdf <- SpatialLinesDataFrame(lines_sp, data=lines_df)
+summary(lines_spdf)
