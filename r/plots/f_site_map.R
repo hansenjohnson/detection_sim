fdbd342317faba2b2576f2b77512ecd649c4fb43
Valentina Ceballos <valentinaceballos@Valentinas-MacBook-Air.local>
2021-07-20 17:33:45 -0300

    Add management zones to map

--- r/plots/f_site_map.R
+++ r/plots/f_site_map.R
@@ -42,19 +42,31 @@ p = ggplot()+
                       breaks = c(seq(from = 0,to = 150, by = 25), 250, 550))+
   # scale_fill_grey(start = 0.9, end = 0.3)+
   scale_fill_brewer(palette = 'Blues') +
+
+  # dynamic management zones
+  annotate('rect', xmin = -62.8, xmax = -62.6, ymin = 48.1, ymax = 48.3, 
+           color = 'orangered4', linetype = 1, fill = 'orangered') +
+  annotate('polygon', x = c(-65,-63.9,-63.9,-65), y = c(49.3,49.1,49.3,49.7), 
+           color = 'chartreuse4', linetype = 1, fill = 'chartreuse3', alpha=.5) +
+  annotate('polygon', x = c(-63.9,-62.9,-62.9,-63.9), y = c(49.1,48.8,49,49.3), 
+           color = 'chartreuse4', linetype = 1, fill = 'chartreuse3', alpha=.5) +
+  annotate('polygon', x = c(-62.9,-62,-62,-62.9), y = c(49,48.6,48.4,48.8), 
+           color = 'chartreuse4', linetype = 1, fill = 'chartreuse3') +
+  annotate('polygon', x = c(-62,-60.9,-61,-61.05,-61.1,-62), y = c(48.6,48.2,48,47.9,48.04,48.4), 
+           color = 'chartreuse4', linetype = 1, fill = 'chartreuse3', alpha=.5) +
+  annotate('polygon', x = c(-64,-63,-63,-64), y = c(50.3,50.3,49.9,50), 
+           color = 'chartreuse4', linetype = 1, fill = 'chartreuse3',alpha=.5) +
   
   # coastline
   geom_sf(data = cf,fill = "cornsilk", color = "cornsilk4", size = 0.1)+
-  
-  # study region
-  annotate('rect', xmin = fmin_lon, xmax = fmax_lon, ymin = fmin_lat, ymax = fmax_lat, 
-           color = 'black', linetype = 2, fill = NA) +
-  
+
   # ocean labels
+  geom_text(aes(x = -63.15, y = 48.3, label = 'DFO', angle = 0), color = 'black', size = sz)+
+  geom_text(aes(x = -63.2, y = 48.95, label = 'TC', angle = 0), color = 'black', size = sz)+
   geom_text(aes(x = -64.5, y = 49.4, label = 'Honguedo Strait', angle = -25), color = 'white', size = sz)+
   geom_text(aes(x = -61, y = 48.5, label = 'Laurentian Channel', angle = 0), color = 'white', size = sz)+
   geom_text(aes(x = -64, y = 50.05, label = 'Jacques-Cartier Strait', angle = -5), size = sz)+
-  geom_text(aes(x = -63.75, y = 48.25, label = 'Orphelin Trough', angle = 65), size = sz)+
+  geom_text(aes(x = -64.15, y = 47.7, label = 'Shediac Valley', angle = 60), size = sz)+
   geom_text(aes(x = -64.6, y = 48.15, label = 'Chaleur Bay', angle = 40), size = sz)+
   geom_text(aes(x = -59.8, y = 47.5, label = 'Cabot Strait', angle = 0), color = 'white', size = sz)+
   geom_text(aes(x = -66.2, y = 49.7, label = 'St Lawrence\nEstuary', angle = 0), color = 'white', size = sz)+
@@ -87,6 +99,8 @@ p = ggplot()+
   theme(legend.position = "right",
         legend.key = element_rect(color = 'black', fill = NA),
         panel.grid = element_blank())
+
+p
 
 # save
 ggsave(
