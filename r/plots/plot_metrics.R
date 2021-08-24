73710fbc8fd140a9b379d284b8c09b29b84907e6
Valentina Ceballos <valentinaceballos@Valentinas-MacBook-Air.local>
2021-08-24 16:08:16 -0300

    Metrics plot labels

--- r/plots/plot_metrics.R
+++ r/plots/plot_metrics.R
@@ -20,10 +20,11 @@ df = readRDS(ifile)
 df$platform = recode(df$platform, slocum = "Slocum glider", plane = "Aircraft", 
                      vessel = "Vessel", rpas = "RPAS")
 
-# pivot df for plotting
+# pivot df and limit whale number for plotting
 dfl = df %>%
   pivot_longer(det_per_hour:cost_per_det, names_to = 'vars', values_to = 'vals') %>%
-  dplyr::select(platform, n_whales, box_type, vars, vals)
+  dplyr::select(platform, n_whales, box_type, vars, vals) %>%
+  dplyr::filter(n_whales <= 20)
 
 # define factors for plotting order
 dfl$vars = factor(dfl$vars, levels = c("det_per_hour", "det_per_dist", 
@@ -34,11 +35,11 @@ dfl$vars = factor(dfl$vars, levels = c("det_per_hour", "det_per_dist",
 dfl$platform = factor(dfl$platform, levels = c("Aircraft","RPAS","Vessel","Slocum glider"), ordered = TRUE)
 
 # prepare metric labels for plotting
-my_labeller = as_labeller(c(cost_per_det="C[d]", 
-                            det_area_time="D[ta]", 
-                            det_per_area="D[a]",
-                            det_per_dist="D[d]",
-                            det_per_hour="D[t]"),
+my_labeller = as_labeller(c(cost_per_det=paste("C[d]  (CAD/detection)"), 
+                            det_area_time="D[ta]   (detections/hr/km^{2})", 
+                            det_per_area="D[a]   (detections/km^{2})",
+                            det_per_dist="D[d]   (detections/km)",
+                            det_per_hour="D[t]   (detections/hr)"),
                            default = label_parsed)
 
 # subset for plotting
@@ -51,13 +52,13 @@ plot_labs_dfo = dfl_dfo %>%
   group_by(vars) %>% 
   summarize(vals = max(vals, na.rm = TRUE)) %>%
   mutate(label = c('a)', 'b)', 'c)', 'd)', 'e)'),
-         n_whales = 60) # set to 0 for left-justified letters
+         n_whales = 20) # set to 0 for left-justified letters
 plot_labs_tc = dfl_tc %>% 
   dplyr::filter(!is.infinite(vals)) %>% # remove infinite cost
   group_by(vars) %>% 
   summarize(vals = max(vals, na.rm = TRUE)) %>%
   mutate(label = c('a)', 'b)', 'c)', 'd)', 'e)'),
-         n_whales = 60) # set to 0 for left-justified letters
+         n_whales = 20) # set to 0 for left-justified letters
 
 # plot
 p = ggplot()+
@@ -106,3 +107,59 @@ metrics_tc = data.frame(t(df_tc))
 # save
 write.csv(metrics_dfo, 'data/processed/metrics_table_dfo.csv')
 write.csv(metrics_tc, 'data/processed/metrics_table_tc.csv')
+
+# make plots will full data set of whales
+dfl_2 = df %>%
+  pivot_longer(det_per_hour:cost_per_det, names_to = 'vars', values_to = 'vals') %>%
+  dplyr::select(platform, n_whales, box_type, vars, vals)
+
+dfl_2$vars = factor(dfl_2$vars, levels = c("det_per_hour", "det_per_dist", 
+                                       "det_per_area", "det_area_time", 
+                                       "cost_per_det"), ordered = TRUE)
+
+dfl_2$platform = factor(dfl_2$platform, levels = c("Aircraft","RPAS","Vessel","Slocum glider"), ordered = TRUE)
+
+dfl_dfo_2 = dfl_2 %>% dplyr::filter(box_type == 'DFO')
+dfl_tc_2 = dfl_2 %>% dplyr::filter(box_type == 'TC')
+
+plot_labs_dfo_2 = dfl_dfo_2 %>% 
+  dplyr::filter(!is.infinite(vals)) %>%
+  group_by(vars) %>% 
+  summarize(vals = max(vals, na.rm = TRUE)) %>%
+  mutate(label = c('a)', 'b)', 'c)', 'd)', 'e)'),
+         n_whales = 60)
+plot_labs_tc_2 = dfl_tc_2 %>% 
+  dplyr::filter(!is.infinite(vals)) %>%
+  group_by(vars) %>% 
+  summarize(vals = max(vals, na.rm = TRUE)) %>%
+  mutate(label = c('a)', 'b)', 'c)', 'd)', 'e)'),
+         n_whales = 60)
+
+r = ggplot()+
+  geom_path(data = dfl_dfo_2, aes(x = n_whales, y = vals, group = platform, color = platform))+
+  geom_text(data = plot_labs_dfo_2, aes(x = n_whales, y = vals, label = label)) +
+  scale_color_manual(values = platform_cols)+
+  labs(x='Number of whales', y='Performance metric value', color = ' Platforms')+
+  facet_wrap(~vars, nrow=3, ncol=2, scales = 'free', labeller=my_labeller)+
+  theme_bw()+
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        panel.border = element_blank())
+
+s = ggplot()+
+  geom_path(data = dfl_tc_2, aes(x = n_whales, y = vals, group = platform, color = platform))+
+  geom_text(data = plot_labs_tc_2, aes(x = n_whales, y = vals, label = label)) +
+  scale_color_manual(values = platform_cols)+
+  labs(x='Number of whales', y='Performance metric value', color = ' Platforms')+
+  facet_wrap(~vars, nrow=3, ncol=2, scales = 'free', labeller=my_labeller)+
+  theme_bw()+
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        panel.border = element_blank())
+
+# save
+ggsave(filename = 'figures/_full_metrics_dfo.png', plot = r, width = 6, height = 8, units = 'in', dpi = 300)
+ggsave(filename = 'figures/_full_metrics_tc.png', plot = s, width = 6, height = 8, units = 'in', dpi = 300)
+
